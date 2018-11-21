bfe008ef738e67f24820129f77ddb9d6910358bd
FengDavid96 <fyhdavid@gmail.com>
2018-11-21 23:22:40 +0100

    login page

--- AndroidStudio/app/src/main/java/fr/fengdavid/matchplayer/MainActivity.java
+++ AndroidStudio/app/src/main/java/fr/fengdavid/matchplayer/MainActivity.java
@@ -2,12 +2,32 @@ package fr.fengdavid.matchplayer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button b1;
+    EditText email,pwd;
+
+    TextView tx1;
+    int attemptcounter=3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        b1 = (Button)findViewById(R.id.button);
+        email = (EditText)findViewById(R.id.editText);
+        pwd = (EditText)findViewById(R.id.editText2);
+
     }
 }

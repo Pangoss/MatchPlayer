98c5108d10c7612a40a8d3ae8ffdfa0bb0a8f0f0
FengDavid96 <fyhdavid@gmail.com>
2018-11-22 01:02:42 +0100

    login page
    login + pwd forgot + register

--- AndroidStudio/app/src/main/java/fr/fengdavid/matchplayer/MainActivity.java
+++ AndroidStudio/app/src/main/java/fr/fengdavid/matchplayer/MainActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.ForegroundColorSpan;
 import android.view.View;
 
 import android.widget.Button;
@@ -25,6 +28,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Change the color of register
+        TextView registerView = findViewById(R.id.txtRegister);
+        String registerText = "Don\'t have an account ? REGISTER HERE";
+
+        SpannableString ss = new SpannableString(registerText);
+        ForegroundColorSpan fgcWhite = new ForegroundColorSpan(Color.WHITE);
+        ss.setSpan(fgcWhite,24,37,Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        registerView.setText(ss);
+
+        // Definition of login page elements
         b1 = (Button)findViewById(R.id.button);
         email = (EditText)findViewById(R.id.editText);
         pwd = (EditText)findViewById(R.id.editText2);

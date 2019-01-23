382365c4a2e0a5a41a199d5ac61d786e0f7109a4
LuisGalanR <lgruiz.97@gmail.com>
2019-01-23 13:11:59 +0100

    Luis created event activity

---
+++ AndroidStudio/app/src/main/java/fr/fengdavid/matchplayer/views/EventActivity.java
@@ -0,0 +1,14 @@
+package fr.fengdavid.matchplayer.views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import fr.fengdavid.matchplayer.R;
+
+public class EventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event);
+    }
+}

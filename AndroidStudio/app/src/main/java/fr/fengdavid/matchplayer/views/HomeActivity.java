4e9772dd21eb7e0f2ac08a0336996c96734f2a6f
carlosgs27 <cgonzalebi@alumnos.unex.es>
2019-01-24 20:46:51 +0100

    HomeActivity edits. SettingsActivity created. build.gradle updated (fixed version dagger)

--- AndroidStudio/app/src/main/java/fr/fengdavid/matchplayer/views/HomeActivity.java
+++ AndroidStudio/app/src/main/java/fr/fengdavid/matchplayer/views/HomeActivity.java
@@ -5,15 +5,30 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import fr.fengdavid.matchplayer.R;
 
 public class HomeActivity extends AppCompatActivity {
 
+    private String username;
+    private String sport;
+    private String level;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Button btn_setting = findViewById(R.id.btn_settings);
+        btn_setting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(HomeActivity.this, SettingsActivity.class);
+                startActivity(i);
+            }
+        });
 
         BottomNavigationView nav =  findViewById(R.id.nav_btn);
 

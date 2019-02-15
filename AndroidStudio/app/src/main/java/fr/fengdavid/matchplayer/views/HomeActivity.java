2638e14ba5c58282c28e2a76fd658168fdb92b61
carlosgs27 <cgonzalebi@alumnos.unex.es>
2019-02-15 12:49:44 +0100

    Search Events upgdated. HomeActivity: profile info, events.

--- AndroidStudio/app/src/main/java/fr/fengdavid/matchplayer/views/HomeActivity.java
+++ AndroidStudio/app/src/main/java/fr/fengdavid/matchplayer/views/HomeActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RatingBar;
 
 import fr.fengdavid.matchplayer.R;
 
@@ -14,7 +16,8 @@ public class HomeActivity extends AppCompatActivity {
 
     private String username;
     private String sport;
-    private String level;
+    private int level;
+    private int nEvents = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +36,8 @@ public class HomeActivity extends AppCompatActivity {
         BottomNavigationView nav =  findViewById(R.id.nav_btn);
 
         nav.setOnNavigationItemSelectedListener(navListener);
+
+
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener navListener =
@@ -44,12 +49,12 @@ public class HomeActivity extends AppCompatActivity {
                             Intent edit = new Intent(HomeActivity.this, ProfileActivity.class);
                             startActivity(edit);
                         break;
-/*
+
                         case R.id.nav_events:
-                            Intent events = new Intent(HomeActivity.this, ProfileActivity.class);
+                            Intent events = new Intent(HomeActivity.this, EventActivity.class);
                             startActivity(events);
                             break;
-*/
+
                         case R.id.nav_search_event:
                             Intent search = new Intent(HomeActivity.this, SearchEventActivity.class);
                             startActivity(search);
@@ -58,5 +63,42 @@ public class HomeActivity extends AppCompatActivity {
                     return false;
                 }
             };
+
+    //Test icons
+    public void onSetIcon (View view) {
+
+        ImageView icon = findViewById(R.id.iv_icon);
+        if(nEvents<4) {
+            nEvents++;
+        } else nEvents = 0;
+
+        RatingBar rb = findViewById(R.id.rb_level);
+        rb.setRating(nEvents+1);
+
+
+        switch (nEvents) {
+            case 0:
+                icon.setImageResource(R.drawable.ic_sport_basket);
+                break;
+
+            case 1:
+                icon.setImageResource(R.drawable.ic_sport_baseball);
+                break;
+
+            case 2:
+                icon.setImageResource(R.drawable.ic_sport_football);
+                break;
+
+            case 3:
+                icon.setImageResource(R.drawable.ic_sport_rugby);
+                break;
+
+            case 4:
+                icon.setImageResource(R.drawable.ic_sport_tennis);
+                break;
+        }
+
+    }
+
 
 }
--- AndroidStudio/app/src/main/java/fr/fengdavid/matchplayer/views/SearchEventActivity.java
+++ AndroidStudio/app/src/main/java/fr/fengdavid/matchplayer/views/SearchEventActivity.java
@@ -1,15 +1,33 @@
 package fr.fengdavid.matchplayer.views;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import fr.fengdavid.matchplayer.R;
 
 public class SearchEventActivity extends AppCompatActivity {
 
+    int nEvents;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_event);
+
+        Button btn_setting = findViewById(R.id.btn_refresh);
+        btn_setting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                searchEvents();
+
+            }
+        });
+    }
+
+    private void searchEvents() {
+
     }
 }
